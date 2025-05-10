ae14ffdb0f19c49604633142f0d5fcef1818cde3
RakibulBh <rakibulbhuiyan1973@gmail.com>
2025-05-10 15:42:02 +0100

    Fix: Updated .gitignore to exclude node_modules and build directories

--- web/.next/static/chunks/app_page_tsx_cf974e51._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_f24c9854._.js",
-  "static/chunks/_d4a2ce75._.js"
-],
-    source: "dynamic"
-});
